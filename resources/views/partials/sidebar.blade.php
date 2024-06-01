8e8d03720ae36556e802df4a4c5c332e25d89569
eldin001 <eldin.balje@icloud.com>
2024-06-01 18:46:43 +0200

    sidebar media query

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,10 +1,18 @@
-<nav id='sidebar'>
-    <div class="admin-cion d-flex align-items-center p-3 gap-3">
-    <a href="/"><i class="fa-solid fa-user-tie fa-2xl"></i></a>
-    <a href="/"><span class="fs-3">Projects Admin</span></a>
-    </div>
-    <ul class="d-flex flex-column gap-4 mt-3">
-        <li><a href=""><i class="fa-solid fa-chart-line fa-lg me-3"></i>Dashboard</a></li>
-        <li><a href=""><i class="fa-regular fa-folder-open fa-lg me-3"></i>Projects</a></li>
-    </ul>
+<nav id="sidebar">
+  <div class="admin-icon d-flex align-items-center p-3 gap-3">
+    <a href="#"><i class="fa-solid fa-user-tie fa-2xl"></i></a>
+    <a href="#"><span class="fs-3">Projects Admin</span></a>
+  </div>
+  <ul class="d-flex flex-column gap-4 mt-3">
+    <li class="{{ (Route::currentRouteName() == 'admin.dashboard' ? 'active' : '') }}">
+      <a href="{{ route('admin.dashboard') }}">
+        <i class="fa-solid fa-chart-line fa-lg me-3"></i><span>Dashboard</span>
+      </a>
+    </li>
+    <li class="{{ (Route::currentRouteName() == 'admin.projects.index' ? 'active' : '') }}">
+      <a href="{{ route('admin.projects.index') }}">
+        <i class="fa-regular fa-folder-open fa-lg me-3"></i><span>Projects</span>
+      </a>
+    </li>
+  </ul>
 </nav>
