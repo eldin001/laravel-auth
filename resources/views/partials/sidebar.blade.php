73e450607956e4636d6da3d007f51eb0d86e86aa
eldin001 <eldin.balje@icloud.com>
2024-06-01 17:24:48 +0200

    style

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,10 @@
-<nav id='sidebar' class="d-flex">
-    <div class="admin-cion">
+<nav id='sidebar'>
+    <div class="admin-cion d-flex align-items-center p-3 gap-3">
     <a href="/"><i class="fa-solid fa-user-tie fa-2xl"></i></a>
-    Dashboard
+    <a href="/"><span class="fs-3">Projects Admin</span></a>
     </div>
-    
-
+    <ul class="d-flex flex-column gap-4 mt-3">
+        <li><a href=""><i class="fa-solid fa-chart-line fa-lg me-3"></i>Dashboard</a></li>
+        <li><a href=""><i class="fa-regular fa-folder-open fa-lg me-3"></i>Projects</a></li>
+    </ul>
 </nav>
